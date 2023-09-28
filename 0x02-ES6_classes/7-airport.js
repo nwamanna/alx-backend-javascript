de18beb642b0798b60a416283747c8db83028b1b
Nwamanna <nwamannazika@gmail.com>
2023-09-29 00:05:13 +0100

    fix code error

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,42 +1,34 @@
 export default class Airport {
-    constructor(name, code) {
-        this.name = name;
-        this.code = code;
-    }
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
 
-    get name() {
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    get code() {
-        return this._code;
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new TypeError('Name must be a string');
     }
-    
+  }
 
-    /**
-     * @param {any} Name
-     */
-    set code(newCode) {
-        if (typeof newCode === "string") {
-            this._code = newCode;
-        } else {
-            throw new TypeError("Code must be a string");
-        }
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode === 'string') {
+      this._code = newCode;
+    } else {
+      throw new TypeError('Code must be a string');
     }
+  }
 
-    set name(newName) {
-        if (typeof newName === "string") {
-            this._name = newName;
-        } else {
-            throw new TypeError("Name must be a string");
-        }
-    }
-
-    // toString() {
-    //     return `Airport [${this.code}] { _name: ${this.name}, _code: ${this.code} }`;
-    // }
-
-    get [Symbol.toStringTag]() {
-        return `${this.code}`;
-    }
+  get [Symbol.toStringTag]() {
+    return `${this.code}`;
+  }
 }
