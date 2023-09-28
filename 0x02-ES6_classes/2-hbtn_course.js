18154f05f043c0d7b9c95e2ce0fa03dad3db05e3
Nwamanna <nwamannazika@gmail.com>
2023-09-28 23:11:29 +0100

    Pair getter and setter

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,46 +1,43 @@
 export default class HolbertonCourse {
-    constructor(name, length, students) {
-        this.name = name;
-        this.length = length;
-        this.students = students;
-    }
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
 
-    get name() {
-        return `${this._name}`;
-    }
+  get name() {
+    return `${this._name}`;
+  }
 
-    get length() {
-        return `${this._length}`;
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new TypeError('Name must be a string');
     }
-    
-    get students() {
-        return `${this._students}`
-    }
+  }
 
-    /**
-     * @param {any} Name
-     */
-    set name(newName) {
-        if (typeof newName === "string") {
-            this._name = newName;
-        } else {
-            throw new TypeError("Name must be a string");
-        }
-    }
+  get length() {
+    return `${this._length}`;
+  }
 
-    set length(newLength) {
-        if (typeof newLength === "number") {
-            this._length = newLength;
-        } else {
-            throw new TypeError("Length must be a number");
-        }
+  set length(newLength) {
+    if (typeof newLength === 'number') {
+      this._length = newLength;
+    } else {
+      throw new TypeError('Length must be a number');
     }
+  }
 
-    set students(newStudent) {
-        if (Array.isArray(newStudent)) {
-            this._students = newStudent;
-        } else {
-            throw new TypeError("Students must be an array");
-        }
+  get students() {
+    return `${this._students}`;
+  }
+
+  set students(newStudent) {
+    if (Array.isArray(newStudent)) {
+      this._students = newStudent;
+    } else {
+      throw new TypeError('Students must be an array');
     }
+  }
 }
