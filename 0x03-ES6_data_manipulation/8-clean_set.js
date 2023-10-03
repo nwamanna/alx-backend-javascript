349154b41ac8c683527dbaaa7e3267635c13ae1c
Nwamanna <nwamannazika@gmail.com>
2023-10-03 18:55:48 +0100

    fix return statement

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,8 @@
 export default function cleanSet(set, word) {
+  if (!word || typeof set !== 'object' || typeof word !== 'string' || word.length === 0) {
+    return '';
+  }
+
   const text = [];
   set.forEach((element) => {
     if (element.startsWith(word) && word.length > 0) {
